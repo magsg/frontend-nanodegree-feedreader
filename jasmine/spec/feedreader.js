3077779f12a67daa882c7689b70dac9ab200c64c
Malgorzata <mw.gradowska@gmail.com>
2018-06-17 16:05:20 +0100

    add Initial entries test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -75,18 +75,29 @@ $(function() {
             expect($("body").hasClass("menu-hidden")).not.toBe(true);
           }
       });
-
     });
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe("Initial entries", function() {
+      /* TODO: Write a test that ensures when the loadFeed
+      * function is called and completes its work, there is at least
+      * a single .entry element within the .feed container.
+      * Remember, loadFeed() is asynchronous so this test will require
+      * the use of Jasmine's beforeEach and asynchronous done() function.
+      */
+      let entries = $('.tpl-entry');
+      beforeEach(function(done) {
+        loadFeed(0);
+        done();
+        });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+      it("are present in the .feed container", function(done) {
+        expect(entries.length).toBeGreaterThan(0);
+        done();
+      });
+    });
+
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
